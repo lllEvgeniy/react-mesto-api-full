3089ffeb017cc0f9ca747ca0f36308e09fd4928b
Evgeniy Mihievich <rashmibalajirashmi@gmail.com>
2022-11-09 18:37:02 +0300

    pr-15

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,7 +6,7 @@ function auth(req, res, next) {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
+    res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
@@ -17,7 +17,7 @@ function auth(req, res, next) {
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return res
+    res
       .status(401)
       .send({ message: 'Необходима авторизация' });
   }
@@ -26,5 +26,29 @@ function auth(req, res, next) {
 
   return next();
 }
+
+
+// try {
+//   const payload = jwt.verify(YOUR_JWT, JWT_SECRET);
+//   console.log('\x1b[31m%s\x1b[0m', `
+//   Надо исправить. В продакшне используется тот же
+//   секретный ключ, что и в режиме разработки.
+//   `);
+// } catch (err) {
+//   if (err.name === 'JsonWebTokenError' && err.message === 'invalid signature') {
+//     console.log(
+//       '\x1b[32m%s\x1b[0m',
+//       'Всё в порядке. Секретные ключи отличаются'
+//     );
+//   } else {
+//     console.log(
+//       '\x1b[33m%s\x1b[0m',
+//       'Что-то не так',
+//       err
+//     );
+//   }
+// }
+
+
 
 module.exports = auth;
